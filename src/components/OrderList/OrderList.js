318e5356c7665b0a8e35550ff6a02f59f5bcdc78
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-04-29 15:50:51 +0600

    added order list

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import './OrderList.css'
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../Firebase.initt";
 
 const OrderList = () => {
+  const [user] = useAuthState(auth);
+  const [orderList, setOrderList] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/orderList`;
+    fetch(url, {
+      headers: {
+        //take email and token
+        authorization: `${user.email} ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setOrderList(data));
+  }, [user.email]);
   return (
-    <div>
-      <h2>OrderList</h2>
+    <div className="container">
+      <h2>OrderList : {orderList.length}</h2>
+      <ol>
+        {orderList.map((order) => (
+          <li>
+            {order.name}
+            <span>
+              <button> </button>
+            </span>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
