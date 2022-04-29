c71bbeb41c5ef669845f421d522bbbe25d7399a4
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-04-29 09:02:24 +0600

    added service from database

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Card } from "react-bootstrap";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/products`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
-    <div>
-      <h2>this is products</h2>
+    <div className="container mt-5">
+      <h2 className="text-center">this is products {products.length}</h2>
+
+      <div className="row g-4 mb-5">
+        {products.map((product) => (
+          <div className="col-4">
+            <Card>
+              <Card.Img variant="top" src={product.image} />
+              <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
