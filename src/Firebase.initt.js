d4fd532e0e5c1fe5697e204dc1a8bfd8ae17ffd6
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-08 08:20:00 +0600

    set Environment firebase

--- src/Firebase.initt.js
+++ src/Firebase.initt.js
@@ -8,12 +8,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJ8B9xbeiuNvIooJkuw_44Ep8K8o_X6GE",
-  authDomain: "good-foods-a594c.firebaseapp.com",
-  projectId: "good-foods-a594c",
-  storageBucket: "good-foods-a594c.appspot.com",
-  messagingSenderId: "98059681592",
-  appId: "1:98059681592:web:b737ee8b99e78a93b0cbcd",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
